d82bf1859933ced8e9d215bf0fb9ede289a945fb
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-11-09 20:57:28 -0300

    Update App with created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
+import './App.css';
+import Logo from './components/Logo';
+import Cards from './components/Cards';
+import Meter from './components/Meter';
+
 export default function App() {
+  const numbers = [14, 7, 5, 20, 2, 9, 34, 11, 8, 1, 14, 7, 5, 20, 2, 9, 34, 11, 8, 1];
+
   return (
-    <h1>!Alzheimer</h1>
+    <>
+      <header>
+        <Logo />
+      </header>
+
+      <main>
+        <Cards numbers={numbers} />
+      </main>
+
+      <footer className="metrics">
+        <Meter label="Tempo" value="0:00" />
+        <Meter label="Movimentos" value="0" />
+      </footer>
+    </>
   );
 }
